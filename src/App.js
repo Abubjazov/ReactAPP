ebdfd29f2f3d3627602386361487eff13de7c9dc
Radik Abubjazov <abubjazov@mail.ru>
2021-09-27 21:07:19 +0300

    тема: события, state и рендеринг

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+import React from 'react';
 import './App.css';
 import Car from './Car/Car';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello React world</h1>
-      <Car name={'Ford'} year={2020} color={'red'} />
-      <Car color={'black'} name={'Audi'} year={2021}>
-      <div style={{color: 'green', }}>Лучшая цена!</div>
-      </Car>
-      <Car color={'black'} name={'Audi'} year={2021}>
-        <p style={{color: 'red'}}>Есть ДТП</p>
-      </Car>
-    </div>
-  )  
+class App extends React.Component {
+
+  state = {
+    cars: [
+      {name: 'Kia', year: 2020, color: 'red'},
+      {name: 'Audi', year: 2015, color: 'black'},
+      {name: 'Mazda', year: 2019, color: 'green'}
+    ],
+    pageTitle: 'Car list'
+  }
+
+ changeTitleHandler = () => {
+    this.setState({
+      pageTitle: 'Changed state'
+    })
+  }
+
+  render() {
+    console.log('Render')
+    const carState = this.state
+    return (
+      <div className='App'>
+        <h1>{carState.pageTitle}</h1>
+  
+        <button onClick={this.changeTitleHandler}>Change title</button>
+  
+        <Car name={carState.cars[0].name} year={carState.cars[0].year} color={carState.cars[0].color} />
+        <Car name={carState.cars[1].name} year={carState.cars[1].year} color={carState.cars[1].color} />
+        <Car name={carState.cars[2].name} year={carState.cars[2].year} color={carState.cars[2].color} />
+      </div>
+    )  
+  }
+  
 }
 
 export default App;
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,40 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
-// function Car(props) {
-//     return (
-//         <div>
-//             <h2>Car name: {props.name}</h2>
-//             <p>Color: {props.color}</p>
-//             <p>Year: {props.year}</p>
-//         </div>
-//     )
-// }
-
-// const car = () => {
-//     return (
-//         <h2>This is Car2</h2>
-//     )
-// }
-
-// const car = () => <h2>This is Car3</h2>
-
-// const car = () => (
-//     <div>
-//         <h2>This is Car3</h2>
-//         <h2>This is Car4</h2>
-//     </div>
-// )
-
-// export default () => (
-//     <div>
-//         <h2>This is Car3</h2>
-//         <h2>This is Car4</h2>
-//     </div>
-// )
 export default props => (
     <div>
         <h2>Car name: {props.name}</h2>
         <p>Color: {props.color}</p>
         <p>Year: {props.year}</p>
-        { props.children }
     </div>
 )
