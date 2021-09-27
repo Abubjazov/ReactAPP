097348ad14f2facc0d45ba4246c5b2808f04e148
Radik Abubjazov <abubjazov@mail.ru>
2021-09-28 02:17:17 +0300

    тема: динамические списки

--- src/App.js
+++ src/App.js
@@ -20,17 +20,22 @@ class App extends React.Component {
     })
   }
 
-  // inputHandler = (event) => {
-  //   this.setState({
-  //     pageTitle: event.target.value
-  //   })
-  // }
+  onChangeName(name, index) {
+    const car = this.state.cars[index]
+    car.name = name
+    // const cars = this.state.cars.concat() //clone array
+    const cars = [...this.state.cars]        //clone array
 
-  // summerTime = () => {
-  //   this.setState({
-  //     pageTitle: new Date().toLocaleString()
-  //   })
-  // }
+    cars[index] = car
+    this.setState({cars})
+  }
+
+  deleteHandler(index) {
+    const cars = this.state.cars.concat()
+
+    cars.splice(index, 1)
+    this.setState({cars})
+  }
 
   render() {
     let cars = null
@@ -43,7 +48,8 @@ class App extends React.Component {
             name={car.name} 
             year={car.year} 
             color={car.color}
-            onChangeTitle={() => this.changeTitleHandler(car.name)}
+            onDelete={this.deleteHandler.bind(this, index)}
+            onChangeName={event => this.onChangeName(event.target.value, index)}
           />
         )
       })
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,9 +1,15 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default props => (
-    <div>
+    <div style={{
+        border: '1px solid #ccc',
+        marginBottom: '10px',
+        display: 'block',
+        padding: '10px'
+    }}>
         <h2>Car name: {props.name}</h2>
         <p>Color: {props.color}</p>
         <p>Year: {props.year}</p>
-        <button onClick={props.onChangeTitle}>Click</button>
+        <input type="text" onChange={props.onChangeName} value={props.name}/>
+        <button onClick={props.onDelete}>Delete</button>
     </div>
 )
