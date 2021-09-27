546d3beaabebd6bc7f0c84ba929ecc3a2a521ccd
Radik Abubjazov <abubjazov@mail.ru>
2021-09-27 19:01:31 +0300

    тема: компоненты

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 // import logo from './logo.svg';
+// import React, { Component } from 'react'
 import './App.css';
+import Car from './Car/Car';
 
 function App() {
   return (
-    <div className="App"></div>
-  );
+    <div className='App'>
+      <h1>Hello React world</h1>
+      <Car />
+    </div>
+  )  
 }
 
 export default App;
+
+//   const styles = {
+//     divStyle: {
+//     'color': 'red',
+//     'text-align': 'center'
+//   },
+//     pStyle: {
+//       'color': 'blue',
+//       'textAlign': 'left'
+//   }
+// }
+  
+
+// function Header(props) {
+//   return (
+//     <div style={props.divStyle}>
+//       <p style={props.pStyle}>Hello React world</p>
+//     </div>
+//   )
+// }
---
+++ src/Car/Car.js
@@ -0,0 +1,28 @@
+function Car() {
+    return (
+        <h2>This is Car</h2>
+    )
+}
+
+// const car = () => {
+//     return (
+//         <h2>This is Car2</h2>
+//     )
+// }
+
+// const car = () => <h2>This is Car3</h2>
+
+// const car = () => (
+//     <div>
+//         <h2>This is Car3</h2>
+//         <h2>This is Car4</h2>
+//     </div>
+// )
+
+// export default () => (
+//     <div>
+//         <h2>This is Car3</h2>
+//         <h2>This is Car4</h2>
+//     </div>
+// )
+export default Car
