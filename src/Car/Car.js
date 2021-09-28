3ae2c2557442fee672fd557ac115667a56925599
Radik Abubjazov <abubjazov@mail.ru>
2021-09-28 21:45:25 +0300

    тема: подключение файла стилей CSS

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,13 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
+import './Car.css' 
 export default props => (
-    <div style={{
-        border: '1px solid #ccc',
-        marginBottom: 10,
-        display: 'block',
-        padding: 10,
-        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .2)',
-        borderRadius: 5
-    }}>
+    <div className='Car'>
         <h2>Car name: {props.name}</h2>
         <p>Color: {props.color}</p>
         <p>Year: {props.year}</p>
