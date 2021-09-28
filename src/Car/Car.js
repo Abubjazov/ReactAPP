a75d9384ba6595bb56b0276a18a2cb08a3fb410a
Radik Abubjazov <abubjazov@mail.ru>
2021-09-28 22:16:30 +0300

    тема: Radium

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
+import Radium from 'radium'
 import './Car.css' 
-export default props => {
+
+const Car = props => {
     const inputClasses = ['input']
 
     if (props.name) {
@@ -12,9 +14,20 @@ export default props => {
     if (props.name.length > 4) {
         inputClasses.push('bold')
     }
-    
+
+    const style = {
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .2)',
+        border: '1px solid #ccc',
+        ':hover': {
+            border: '2px solid #aaa',
+            boxShadow: '0 4px 15 px rgba(0, 0, 0, .25)',
+            backgroundColor:'yellow',
+            cursor: 'pointer'
+        }
+    }
+
     return (
-    <div className='Car'>
+    <div className='Car' style={style}>
         <h2>Car name: {props.name}</h2>
         <p>Color: {props.color}</p>
         <p>Year: {props.year}</p>
@@ -28,3 +41,5 @@ export default props => {
     </div>
 )
     }
+
+export default Radium(Car)
