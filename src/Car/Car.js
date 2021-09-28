2ba23f415caf0375a7196a0cbfc5b5b350323459
Radik Abubjazov <abubjazov@mail.ru>
2021-09-28 21:57:06 +0300

    тема: динамическое подключение классов CSS

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,11 +1,30 @@
 /* eslint-disable import/no-anonymous-default-export */
 import './Car.css' 
-export default props => (
+export default props => {
+    const inputClasses = ['input']
+
+    if (props.name) {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if (props.name.length > 4) {
+        inputClasses.push('bold')
+    }
+    
+    return (
     <div className='Car'>
         <h2>Car name: {props.name}</h2>
         <p>Color: {props.color}</p>
         <p>Year: {props.year}</p>
-        <input type="text" onChange={props.onChangeName} value={props.name}/>
+        <input 
+        type="text" 
+        onChange={props.onChangeName} 
+        value={props.name}
+        className={inputClasses.join(' ')}
+        />
         <button onClick={props.onDelete}>Delete</button>
     </div>
 )
+    }
