0a0d20b7de837712603e8f51b8490b6390fd9ba8
Radik Abubjazov <abubjazov@mail.ru>
2021-10-01 20:55:19 +0300

    тема: Routing - параметры ссылок

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,25 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react"
+import { NavLink } from "react-router-dom"
 import "./Header.css"
 
 export default props => {
     return (
         <div>
-            <a href="/" className="Header">Home</a>
-            <a href="/about" className="Header">About</a>
-            <a href="/cars" className="Header">Cars</a>                     
+            <NavLink to="/" 
+            exact 
+            activeClassName={'wfm-active'} 
+            className="Header"
+            >Home</NavLink>
+            <NavLink to="/about" className="Header" activeStyle={{color: 'blue'}}>About</NavLink>
+            <NavLink 
+            to={{
+                pathname: '/cars',
+                search: '?a=1&b=2',
+                hash: 'wfm-hash'
+            }} 
+            className="Header"
+            >Cars</NavLink>                     
         </div>
     )
 }
